a5147930b699443cb67789c75e4cf0881f6b5b54
Nicolas Florencio Alves <nicolas.alves14@etec.sp.gov.br>
2021-04-01 15:45:06 -0300

    Form 12 update

--- php - etec/exercicios/form12.php
+++ php - etec/exercicios/form12.php
@@ -10,21 +10,16 @@
 <meta charset="UTF-8">
     <!-- Ler o nome e a quant. de gols marcados por eles caso haja empate exiba EMPATE-->
 
-<<<<<<< HEAD
-    <form action="res12.php" method="get">
-=======
     <p>Informe o nome do time 1: </p><br><input type="text" name="name1"/> <br>
     <p>Informe o nome do time 2: </p><br><input type="text" name="name2"/> <br>
     <p>Informe a quantidade de gols marcados pelo time 1 </p><br><input type="number" step="any" name="gol1"/> <br>
     <p>Informe a quantidade de gols marcados pelo time 2 </p><br><input type="number" step="any" name="gol2"/> <br>
->>>>>>> 19d9aa713dd9d35e96ed51ea187f7d914532855e
 
         Informe o nome do time 1: <br><input type="text" name="name1"/> <br>
         Informe o nome do time 2: <br><input type="text" name="name2"/> <br>
         Informe a quantidade de gols marcados pelo time 1 <br><input type="number" step="any" name="gol1"/> <br>
         Informe a quantidade de gols marcados pelo time 2 <br><input type="number" step="any" name="gol2"/> <br>
 
-<<<<<<< HEAD
         <br> <input type="submit" value="Comparar"/>
         
     </form>
@@ -33,7 +28,3 @@
     <li> <a href="../index.html"> Voltar para o Index de atividades </a></li>
 </body>
 </html>
-=======
-<li> <a href="index.php"> Voltar para Exercícios em PHP </a></li> <br>
-<li> <a href="../index.html"> Voltar para o Index de atividades </a></li>
->>>>>>> 19d9aa713dd9d35e96ed51ea187f7d914532855e

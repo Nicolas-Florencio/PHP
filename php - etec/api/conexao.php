85b68ce60e553733d269467dc97e8baf532b1dfa
Nicolas Florencio Alves <nicolas.alves14@etec.sp.gov.br>
2021-05-23 18:23:33 -0300

    API Rest com PHP

---
+++ php - etec/api/conexao.php
@@ -0,0 +1,10 @@
+<?php
+    $conexao = new mysqli("localhost:3306", "root", "123456");
+    $conexao->set_charset("UTF8");
+
+    if ($conexao->connect_error) {
+        die("Falha ao conectar: " . $conexao->connect_error);
+    }
+    if (!$conexao->select_db("api_produto")) {
+        die("O Banco de dados não existe");
+    }
